6c7e17bb68de9a1a79fdcbc985eea7611e9a0f98
Raphael Oliveira <raphaelfpoliveira@gmail.com>
2012-05-04 10:46:09 -0300

    Criando o modelo da aplicacao

---
+++ people/models.py
@@ -0,0 +1,18 @@
+from django.db import models
+
+class Usuario(models.Model):
+    email = models.EmailField(max_length=200)
+
+    def __unicode__(self):
+    	return self.email
+
+class Contato(models.Model):
+    nome = models.CharField(max_length=200)
+    telefone = models.CharField(max_length=200)
+
+    def __unicode__(self):
+    	return self.nome + ': '+ self.telefone
+
+class Agenda(models.Model):
+    usuario = models.ForeignKey(Usuario)
+    contatos = models.ManyToManyField(Contato)
